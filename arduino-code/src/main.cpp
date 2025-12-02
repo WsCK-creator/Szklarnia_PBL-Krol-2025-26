86dfddbf201ee7d1c99bae9f0892314c1428147b
Maurycy Frydecki <maurycy.frydecki@gmail.com>
2025-12-02 20:39:07 +0100

    Dodałem obsługę enkodera

--- arduino-code/src/main.cpp
+++ arduino-code/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include "Enkoder.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+#define ENCODER_CLK_PIN 3
+#define ENCODER_DT_PIN 34
+#define ENCODER_SW_PIN 2
+#define DTH11_IN_PIN 32
+#define DTH11_OUT_PIN 33
+#define SOIL_SENSOR_1_PIN A0
+#define SOIL_SENSOR_2_PIN A1
+#define SOIL_SENSOR_3_PIN A2
+#define LED_R_PIN 41
+#define LED_G_PIN 39
+#define LED_B_PIN 40
+#define RELAY_ACTUATOR_PIN 46
+#define RELAY_DIRECTION_PIN 45
+#define RELAY_PUMP_PIN 44
+#define RELAY_LED_PIN 43
+#define RELAY_HEATER_PIN 42
+
+
+
+Enkoder enkoder(ENCODER_CLK_PIN, ENCODER_DT_PIN, ENCODER_SW_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  enkoder.Init(encBtPressed, encTurned);//add callback functions
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  enkoder.loop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void encBtPressed() {
+  // Handle button press
+}
+void encTurned(int position) {
+  // Handle encoder turn
 }
